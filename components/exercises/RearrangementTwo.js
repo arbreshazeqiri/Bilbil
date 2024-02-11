96378e93751a68ddb7b1b33a151a3a9ed6102077
arbreshazeqiri <arbresha.zeqiri@student.uni-pr.edu>
2024-02-11 12:03:09 +0100

    fixes

--- components/exercises/RearrangementTwo.js
+++ components/exercises/RearrangementTwo.js
@@ -5,9 +5,9 @@ const RearrangementTwo = () => {
   const [storageWords, setStorageWords] = useState([
     { id: 1, text: "Unë" },
     { id: 2, text: "zjarrin" },
-    { id: 3, text: "coding" },
+    { id: 3, text: "filler" },
     { id: 4, text: "ndez" },
-    { id: 5, text: "coding" },
+    { id: 5, text: "filler2" },
   ]);
 
   const [placementWords, setPlacementWords] = useState([]);
@@ -35,55 +35,87 @@ const RearrangementTwo = () => {
       onPanResponderRelease: (event, gesture) => {
         const { id: currentId } = word;
         const currentPosition = wordPositions[currentId];
-      
-        // Determine if the word was released in the storage or placement area
-        const isReleasedInPlacement = gesture.moveY > 100; // Adjust the threshold according to your UI
-      
-        if (isReleasedInPlacement) {
-          // Check if the word should be moved from storage to placement or rearranged within placement
+        const isDragged = gesture.dx !== 0 || gesture.dy !== 0;
+
+        if (!isDragged) {
           const updatedPlacementWords = [...placementWords];
-          const existingWordIndex = updatedPlacementWords.findIndex((w) => w.id === currentId);
-      
+          const existingWordIndex = updatedPlacementWords.findIndex(
+            (w) => w.id === currentId
+          );
+
           // Remove the word from its current position in the placement area, if it exists
           if (existingWordIndex !== -1) {
             updatedPlacementWords.splice(existingWordIndex, 1);
           }
-      
-          // Find the index where the word should be inserted in the placement area
-          let insertionIndex = updatedPlacementWords.findIndex((w) => {
-            const wordPosition = wordPositions[w.id];
-            return (
-              wordPosition.x._value > currentPosition.x._value && wordPosition.y._value > currentPosition.y._value
+
+          if (!placementWords.includes(word)) {
+            updatedPlacementWords.push(word);
+            setPlacementWords(updatedPlacementWords);
+
+            // Remove the word from the storage area
+            const updatedStorageWords = storageWords.filter(
+              (w) => w.id !== currentId
             );
-          });
-      
-          // If the insertion index is -1, it means the word is dropped at the end
-          if (insertionIndex === -1) {
-            insertionIndex = updatedPlacementWords.length;
+            setStorageWords(updatedStorageWords);
+          } else {
+            updatedPlacementWords.filter((w) => w.id !== currentId);
+            setPlacementWords(updatedPlacementWords);
+
+            // Remove the word from the storage area
+            const updatedStorageWords = [...storageWords, word];
+            setStorageWords(updatedStorageWords);
           }
-      
+          // Insert the word at the end of the placement area
+        } else {
+          // Determine if the word was released in the storage or placement area
+          const isReleasedInPlacement = gesture.moveY > 100; // Adjust the threshold according to your UI
+
+          if (isReleasedInPlacement) {
+            // Check if the word should be moved from storage to placement or rearranged within placement
+            const updatedPlacementWords = [...placementWords];
+            const existingWordIndex = updatedPlacementWords.findIndex(
+              (w) => w.id === currentId
+            );
+
+            // Remove the word from its current position in the placement area, if it exists
+            if (existingWordIndex !== -1) {
+              updatedPlacementWords.splice(existingWordIndex, 1);
+            }
+
+            const isMovementLeftToRight = gesture.dx > 0;
+
+            // Find the index where the word should be inserted in the placement area
+            let insertionIndex = updatedPlacementWords.findIndex((w) => {
+              const wordPosition = wordPositions[w.id];
+              const wordRight = wordPosition.x._value + w.text.length + 10;
+          
+              if (isMovementLeftToRight) {
+                  // For left-to-right movement
+                  return wordPosition.x._value > currentPosition.x._value || wordPosition.y._value > currentPosition.y._value;
+              } else {
+                  // For right-to-left movement
+                  return wordRight < currentPosition.x._value || wordPosition.y._value < currentPosition.y._value;
+              }
+          });
+          
+          // If no word is surpassed, leave it where it is
+          if (insertionIndex === -1) {
+              insertionIndex = updatedPlacementWords.length; // Insert at the end
+          }
+          
           // Insert the word at the appropriate index
           updatedPlacementWords.splice(insertionIndex, 0, word);
-          setPlacementWords(updatedPlacementWords);
-      
-          // Remove the word from the storage area
-          const updatedStorageWords = storageWords.filter((w) => w.id !== currentId);
-          setStorageWords(updatedStorageWords);
-        } else {
-          // Check if the word should be moved from placement to storage or rearranged within storage
-          const updatedStorageWords = [...storageWords];
-          const existingWordIndex = updatedStorageWords.findIndex((w) => w.id === currentId);
-      
-          // Remove the word from its current position in the storage area, if it exists
-          if (existingWordIndex !== -1) {
-            updatedStorageWords.splice(existingWordIndex, 1);
+          
+            setPlacementWords(updatedPlacementWords);
+            setWordPositions({...wordPositions});
+            // Remove the word from the storage area
+            const updatedStorageWords = storageWords.filter(
+              (w) => w.id !== currentId
+            );
+            setStorageWords(updatedStorageWords);
           }
-      
-          // Insert the word at the end of the storage area
-          updatedStorageWords.push(word);
-          setStorageWords(updatedStorageWords);
         }
-      }, 
+      },
     });
   });
 
@@ -99,7 +131,7 @@ const RearrangementTwo = () => {
         ]}
         {...panResponders[word.id].panHandlers}
       >
-        <Text>{word.text}</Text>
+        <Text style={{ backgroundColor: 'yellow'}}>{word.text}</Text>
       </Animated.View>
     ));
   };
@@ -126,23 +158,32 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
     backgroundColor: "#9d8c8c",
+    paddingVertical: 50,
+    paddingHorizontal: 10,
   },
   storageArea: {
     alignItems: "center",
+    width: "100%",
+    backgroundColor: "blue",
   },
   placementArea: {
     alignItems: "start",
+    width: "100%",
+    backgroundColor: "red",
+    paddingHorizontal: 50,
   },
   wordsContainer: {
+    width: "100%",
     flexDirection: "row",
     alignItems: "center",
     flexWrap: "wrap",
+    gap: 20,
+    zIndex: 1,
   },
   word: {
     backgroundColor: "#eee",
     padding: 10,
     borderRadius: 5,
-    margin: 5,
   },
   title: {
     fontSize: 18,
