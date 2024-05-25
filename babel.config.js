ad2516b0bf0b037acfc057e0fcd39bbbde9f5975
arbreshazeqiri <arbresha.zeqiri@student.uni-pr.edu>
2024-05-25 22:16:40 +0200

    fix

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
-
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: ["babel-preset-expo"],
     plugins: [
       [
         "module:react-native-dotenv",
         {
           moduleName: "@env",
           path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
         },
       ],
       "react-native-reanimated/plugin",
-      "@babel/plugin-transform-react-jsx-source",
-      "@babel/plugin-transform-class-properties",
-      '@babel/plugin-transform-private-methods'
     ],
   };
 };
